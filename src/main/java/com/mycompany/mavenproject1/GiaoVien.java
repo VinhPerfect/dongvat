80f724142334be97b6a0305bb2c6a959d3da7a99
VinhPerfect <lqv.209@gmail.com>
2024-05-20 09:10:57 +0700

    First Commit

---
+++ src/main/java/com/mycompany/mavenproject1/GiaoVien.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author lqv20
+ */
+public class GiaoVien {
+   public static void main(String[] args){
+   
+   } 
+}
